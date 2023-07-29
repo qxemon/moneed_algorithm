7f8c565d96fe9a590f75664f24daecf1e9724e6d
Moneed <qxemon@gmail.com>
2023-07-29 21:21:57 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//나머지
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[10];
+        int[] remainder = new int[42]; // 나머지 0~41
+        int rem=0;
+        //입력
+        for(int i=0; i<num.length;i++){
+            num[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        for(int i=0;i< num.length;i++){
+            rem = num[i] % 42;
+            if(remainder[rem] ==0){
+                count++;
+                remainder[rem] = 1;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
