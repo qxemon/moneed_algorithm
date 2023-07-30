2b5bbb9b3ce09a18bbf088f75223dc2e1e9e3e2f
박예원 <parkyw0321@gmail.com>
2023-07-30 23:18:18 +0900

    SWEA1926. 간단한 369게임

---
+++ JAVA/SWEA/src/D2/SWEA1926.java
@@ -0,0 +1,28 @@
+package D2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA1926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean is369 = false;
+        for(int i =1; i<=n; i++){
+            is369 = false;
+            int g = i;
+            while(g > 0){
+                if(g%10 == 3 || g%10 == 6 || g%10 == 9){
+                    is369 = true;
+                    System.out.print("-");
+                }
+                g /= 10;
+            }
+            if(is369)
+                System.out.print(" ");
+            else
+                System.out.print(i+" ");
+        }
+    }
+}
