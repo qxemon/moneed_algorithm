1ee39956605335a9d560bce697bc999dacb3da73
박예원 <parkyw0321@gmail.com>
2023-08-03 20:05:52 +0900

    BOJ2304. 창고다각형

--- JAVA/BOJ/src/BOJ2304.java
+++ JAVA/BOJ/src/BOJ2304.java
@@ -1,23 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
+//창고다각형
 public class BOJ2304 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[] ls = new int[1001];//위치 저장 배열(범위가 1~1000이하 이므로 그냥 1001짜리 만듦)
+        int N = Integer.parseInt(br.readLine()); // 기둥 개수
 
-        int[] x = new int[N];
-        int[] y = new int[N];
-
-        for(int i=0;i<N;i++){
-            x[i] = sc.nextInt();
-            y[i] = sc.nextInt();
+        //입력
+        for(int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());//위치
+            int H = Integer.parseInt(st.nextToken());//높이
+            ls[L] = H;
         }
-        System.out.println(Arrays.toString(x));
-        System.out.println(Arrays.toString(y));
+
+        //최대 높이 인덱스 찾기
+        int max = 0;
+        int maxIndex = 1;
+        for(int i=1; i<=1000;i++) {
+            if(max < Math.max(ls[i], max)) {
+                max = ls[i];
+                maxIndex = i;
+            }
+        }
+
+        int highL = 0;
+        int sumLeft = 0;
+        //1. 최대 높이 인덱스 왼쪽 합(최대높이 이전)
+        for(int i=1;i<maxIndex;i++) {
+            if(highL < ls[i]) {
+                highL = ls[i];
+            }
+            sumLeft += highL;
+        }
 
 
+        int highR = 0;
+        int sumRight = 0;
 
+        //2. 최대 높이 인덱스 오른쪽 합(최대높이까지)
+        for(int i=1000; i>=maxIndex;i--) {
+            if(highR < ls[i]) {
+                highR = ls[i];
+            }
+            sumRight += highR;
+        }
 
+        int area = sumLeft + sumRight;
+        System.out.println(area);
     }
 }
