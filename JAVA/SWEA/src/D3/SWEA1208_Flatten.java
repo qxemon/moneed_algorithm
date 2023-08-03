0da6ef89047531613ad35866ca83ba0286694bbf
박예원 <parkyw0321@gmail.com>
2023-08-03 20:46:02 +0900

    SWEA1208_Flatten

---
+++ JAVA/SWEA/src/D3/SWEA1208_Flatten.java
@@ -0,0 +1,39 @@
+package D3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SWEA1208_Flatten {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for(int tc=1;tc<=10; tc++){
+            int dump = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] boxes = new int[100];
+            for(int i=0;i<100;i++){
+                boxes[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for(int i=0;i<dump;i++){
+                Arrays.sort(boxes);
+                boxes[99]--;
+                boxes[0]++;
+
+                if(boxes[99] - boxes[0] <= 1){
+                    break;
+                }
+            }
+            Arrays.sort(boxes);
+
+            System.out.println("#"+tc+" "+(boxes[99]-boxes[0]));
+        }
+
+
+
+
+    }
+}
