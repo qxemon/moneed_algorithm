c6ff75a1b830a7bebc067b5142e6a6b866ff7cea
박예원 <parkyw0321@gmail.com>
2023-08-07 21:03:50 +0900

    BOJ2164_카드2

---
+++ JAVA/BOJ/src/Silver/BOJ_2164_카드2.java
@@ -0,0 +1,26 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class BOJ_2164_카드2 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ArrayDeque<Integer>queue = new ArrayDeque<>();
+		int N = Integer.parseInt(br.readLine());
+		for(int i = N; i>0;i--) {
+			queue.add(i);
+		}
+
+		while(true){
+			if(queue.size() == 1) break;
+			queue.pollLast();
+			int move = queue.pollLast();
+			queue.addFirst(move);
+		}
+
+		System.out.println(queue.pop());
+	}
+}
