49ff0f590ad5c7a12cb8c73de31244d4ce675ca4
Moneed <qxemon@gmail.com>
2023-08-30 00:46:51 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 80 ms, Memory: 11888 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -7,58 +7,55 @@ public class Main {
 
     static int[][] map;
     static long[][][] memo;
-    static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
 
-        map = new int[N + 1][N + 1];
+        map = new int[N][N];
+        memo = new long[N][N][3]; //0: 가로로 오는 것 1: 대각선 오는 것, 2: 세로로 오는 것 개수 저장할거임
+
+        memo[0][1][0] = 1;// 초기화
 
 
-
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-
-        if (map[N][N] == 1) {
+        if(map[N-1][N-1] == 1){
             System.out.println(0);
             return;
         }
 
-        memo = new long[N + 1][N + 1][3]; //0: 가로로 오는 것 1: 대각선 오는 것, 2: 세로로 오는 것 개수 저장할거임
 
-        memo[1][2][0] = 1;// 초기화
+        for (int i = 0; i < N; i++) {
+            for (int j = 2; j < N; j++) {
+                if(map[i][j] == 1){
+                    memo[i][j][0] = memo[i][j][1]= memo[i][j][2] = 0;
+                }
+                else{
+                    memo[i][j][0] = memo[i][j-1][0] +memo[i][j-1][1];
+                    if(i>=1){
+                        if(map [i-1][j] != 1 && map[i][j-1] != 1){
+                            memo[i][j][1] = memo[i-1][j-1][0]+memo[i-1][j-1][1]+memo[i-1][j-1][2];
+                        }
+                        memo[i][j][2] = memo[i-1][j][1]+memo[i-1][j][2];
+                    }
 
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 3; j <= N; j++) {
-                if (map[i][j] == 1)
-                    continue;
-
-                memo[i][j][0] = memo[i][j - 1][0] + memo[i][j - 1][1]; // 가로 처리
-                if (i == 1) continue;
-                memo[i][j][2] = memo[i - 1][j][1] + memo[i - 1][j][2]; //세로 처리
-
-                if (map[i - 1][j] == 1 || map[i][j - 1] == 1) continue;
-
-                memo[i][j][1] = memo[i - 1][j - 1][0] + memo[i - 1][j - 1][1] + memo[i - 1][j - 1][2];
-
-
-
+                }
+            }
         }
-    }
 
-     long ans = memo[N][N][0] + memo[N][N][1] + memo[N][N][2];
+        long ans = memo[N-1][N-1][0] + memo[N-1][N-1][1] + memo[N-1][N-1][2];
 
         System.out.println(ans);
 
 
-}
+
+
+    }
 
 }
