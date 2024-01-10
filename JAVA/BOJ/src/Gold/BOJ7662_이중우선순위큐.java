f9b67fb77a0eb2d21dd007b361a00b2c16216fa6
박예원 <qxemon@gmail.com>
2024-01-10 22:15:25 +0900

    [fix] map, pq, treemap

--- JAVA/BOJ/src/Gold/BOJ7662_이중우선순위큐.java
+++ JAVA/BOJ/src/Gold/BOJ7662_이중우선순위큐.java
@@ -1,55 +1,76 @@
 package Gold;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ7662_이중우선순위큐 {
+
+    static Map<Integer, Integer> map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
-            PriorityQueue<Integer> pqs = new PriorityQueue<>(); //오름차순 pq
-            PriorityQueue<Integer> pqb = new PriorityQueue<>(((o1, o2) -> {
-                return o2-o1;
-            })); // 내림차순
-
             int input = Integer.parseInt(br.readLine());
 
-            int inp = 0;
-            int del = 0;
+            PriorityQueue<Integer> pqs = new PriorityQueue<>(); //오름차순 pq
+            PriorityQueue<Integer> pqb = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
+            map = new HashMap<>(); //각 숫자를 기억할 맵
 
             for (int i = 0; i < input; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 String cal = st.nextToken();
+
                 int num = Integer.parseInt(st.nextToken());
                 if(cal.equals("I")){
                     pqs.add(num);
                     pqb.add(num);
-                    inp++;
+                    map.put(num,map.getOrDefault(num,0)+1);
+
                 } else{
-                    if (num == -1) { // 최솟값 제거
-                        pqs.poll();
+
+                    if(map.size()==0) continue;
+
+                    if (num == 1) { // 최댓값 제거
+                        delete(pqb);
                     }
-                    else if (num == 1){ //최댓값 제거
-                        pqb.poll();
+                    else{ //최솟값 제거
+                        delete(pqs);
                     }
-                    del++;
+
                 }
             }
 
-            if(inp <= del){
-                sb.append("EMPTY").append("\n");
-            }
+            if(map.size()==0) sb.append("EMPTY\n");
             else{
-                sb.append(pqb.poll()).append(" ").append(pqs.poll()).append("\n");
+                int res = delete(pqb);
+                sb.append(res).append(" ");
+                if(map.size()>0) res = delete(pqs);
+                sb.append(res).append("\n");
             }
         }
 
         System.out.println(sb.toString());
     }
+    public static int delete(Queue<Integer> pq){
+        int n =0;
+        while(true){
+            n = pq.poll();
+
+            int cnt = map.getOrDefault(n, 0);
+            if(cnt == 0) continue;
+
+            if(cnt == 1) map.remove(n);
+            else map.put(n,cnt-1);
+            break;
+        }
+        return n;
+    }
+
 }
