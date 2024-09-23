f2913df810a15a6e59be4cc2ce0cc32476787e12
park yewon <qxemon@gmail.com>
2024-09-23 17:17:07 +0900

    feat(2024_09): 중복 순열, 예외처리, 브루트포스

--- JAVA/BOJ/src/Gold/BOJ1107_리모컨.java
+++ JAVA/BOJ/src/Gold/BOJ1107_리모컨.java
@@ -8,79 +8,64 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ1107_리모컨 {
-    static int N, M;
-    static int nLen;
-    static int[] numbers;
-    static int[] broken;
-    static int[] result;
+    static boolean[] broken = new boolean[10];
+    static int channel;
     static int ans;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        nLen = s.length();
-        N = Integer.parseInt(s);
-        M = Integer.parseInt(br.readLine());
-        if (M == 0){
-            numbers = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        }
-        else{
+        channel = Integer.parseInt(s);
+        int broken_num = Integer.parseInt(br.readLine());
+
+        //BufferedReader NullPointer Exception 방지를 위함
+        if (broken_num > 0) {
+
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            broken = new int[M];
-            numbers = new int[10-M];
-            for (int i = 0; i < M; i++) {
-                broken[i] =Integer.parseInt(st.nextToken());
+            for (int i = 0; i < broken_num; i++) {
+                broken[Integer.parseInt(st.nextToken())] = true;
             }
-            int idx = 0;
-ex:         for(int i=0; i < 10; i++){
-                for (int j = 0; j < M; j++) {
-                    if(broken[j] == i){
-                        continue ex;
-                    }
-                }
-                numbers[idx++] = i;
-            }
+
         }
 
-        if(N == 100){ // 예외, 첫 채널 100 -> 이동 횟수 0
+        if (channel == 100) {
             System.out.println(0);
             return;
         }
 
-//        System.out.println(Arrays.toString(numbers));
-        result = new int[nLen];
         ans = Integer.MAX_VALUE;
-        perm(0);
 
-        System.out.println(ans + nLen);
+        //1. 100번 대에서 +, - 만 눌러 도달하는 횟수
+        ans = Math.abs(channel - 100);
+
+        //2. 채널 조합 후 (1~ 500000 ) 1~6자리 확인
+        for (int i = 1; i <= 6; i++) {
+            perm(0, i, 0);
+        }
+
+        System.out.println(ans);
 
     }
-    static void perm(int idx){
-        if(idx == nLen){
-//            System.out.println(Arrays.toString(result));
 
-            int num = 0;
-            //숫자 조합하자
-            for (int i = 0; i < nLen; i++) {
-                num *= 10;
-                num += result[i];
+    static void perm(int pick, int len, int num) {
+        if (pick == len) {
 
-            }
+            System.out.println(num);
 
-//            System.out.println(num);
-            int res = Math.abs(num - N);
-            ans = Math.min(res, ans);
+            int count = Math.abs(channel - num) + len;
+
+            ans = Math.min(ans, count);
 
             return;
         }
 
-        for(int i=0; i<numbers.length; i++){
-            result[idx] = numbers[i];
-            perm(idx+1);
+
+        for (int i = 0; i < 10; i++) {
+            if (!broken[i]) {
+                perm(pick + 1, len, num * 10 + i);
+            }
         }
-
-
-
 
     }
 
