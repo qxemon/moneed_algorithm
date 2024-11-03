f906dc11289261793cf2ab3981a538fe06912757
park yewon <qxemon@gmail.com>
2024-11-03 22:57:55 +0900

    feat(2024_11): greedy, sort

---
+++ JAVA/BOJ/src/Silver/BOJ11399_ATM.java
@@ -0,0 +1,32 @@
+package Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11399_ATM {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int count = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            count += arr[i];
+            ans += count;
+        }
+
+        System.out.println(ans);
+
+
+    }
+}
